59c39f53f300d755c4165d68848eea3504fddb6f
Edgaras <edgaras@apsega.lt>
2015-12-08 10:12:27 -0500

    initial commit

---
+++ docker_script.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#Declare log filename and location
+log=/tmp/docker_cleaner_$(date +"%m-%d-%y").log
+
+#Get active container names
+active=$(docker ps -a --format "{{.Names}}" --filter status=running)
+
+#Get volumes names of active containers
+active_volumes=$(docker inspect $active | grep "Source" | grep /var/lib/docker | awk -F '"'  '{print $4}' | awk -F '/' '{print $6}')
+
+#Pass used and all directory names to arrays
+read -a used_directories <<< $active_volumes
+read -a all_directories <<< $(ls /var/lib/docker/volumes)
+
+#Remove used directories from all directories array
+for del in ${used_directories[@]}
+do
+   all_directories=(${all_directories[@]/$del})
+done
+
+if [ ${#all_directories[@]} -eq 0 ]; then
+    echo "Nothing to delete"
+else
+    echo "Deleting..." | tee $log
+    cd /var/lib/docker/volumes && rm -rfv ${all_directories[@]} >> $log
+    echo "Done" | tee -a $log	
+fi
